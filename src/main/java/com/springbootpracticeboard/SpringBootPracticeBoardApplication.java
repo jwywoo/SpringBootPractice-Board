6c7294fb5bd57e82da458fb5faa39377a27c65a1
jwywoo <jwywoo26@gmail.com>
2024-01-17 16:56:39 +0900

    #1 Feat: Project Basic Setting
    Resolves: None
    Ref: None
    Related to: #1

---
+++ src/main/java/com/springbootpracticeboard/SpringBootPracticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.springbootpracticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootpracticeboard/SpringBootPracticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootpracticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
